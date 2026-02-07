4752fdbe6a3e9d64b8ea03af28e3af475247c4d3
LeanBitLab <admin@leanbitlab.com>
2026-02-07 17:35:19 +0530

    Bump version to 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.leanbitlab.lwidget"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     signingConfigs {
