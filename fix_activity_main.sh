e5a20172832f89fe7e29992733bca82b4e2c2eea
LeanBitLab <admin@leanbitlab.com>
2026-05-03 00:42:53 +0530

    chore: clean up unwanted patch, diff, orig, and script files

--- fix_activity_main.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sed -i '/android:id="@+id\/row_date_color"/,+4d' app/src/main/res/layout/activity_main.xml
-sed -i '/android:id="@+id\/row_date_color_custom"/,+4d' app/src/main/res/layout/activity_main.xml
